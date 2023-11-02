13a93a365729bba042f9755ba5ec3a2223c8fd54
Ishan9970 <142791726+Ishan9970@users.noreply.github.com>
2023-11-02 18:38:23 +0530

    script.js

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-// Array of tulip image URLs
-var tulipImages = [
-    "tulips1.jpg",
-    "tulips2.jpg",
-    "tulips3.jpg",
-    "tulips4.jpg",
-    // Add more image URLs here
-];
-
-var bouquetContainer = document.getElementById("bouquet");
-
-// Function to add tulips to the bouquet
-function addTulip() {
-    var tulipImage = document.createElement("img");
-    tulipImage.src = tulipImages[Math.floor(Math.random() * tulipImages.length)];
-    tulipImage.className = "tulip";
-    bouquetContainer.appendChild(tulipImage);
+function setup() {
+  createCanvas(400, 400);
+  background(200);
+  drawTulip(width / 2, height / 2);
 }
 
-// Add tulips to the bouquet
-for (var i = 0; i < 10; i++) { // You can adjust the number of tulips
-    addTulip();
+function drawTulip(x, y) {
+  // Stem
+  fill(34, 139, 34);
+  rect(x - 5, y, 10, 100);
+
+  // Leaves
+  fill(0, 128, 0);
+  ellipse(x - 10, y + 20, 30, 20);
+  ellipse(x + 10, y + 20, 30, 20);
+
+  // Flower
+  fill(255, 0, 0);
+  ellipse(x, y, 40, 40);
+  fill(255, 192, 203);
+  ellipse(x - 15, y, 30, 30);
+  ellipse(x + 15, y, 30, 30);
 }
- 
