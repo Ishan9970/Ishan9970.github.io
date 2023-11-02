2e258395c4a084ab4bbbcb88edea9cd93bdc62bd
Ishan9970 <142791726+Ishan9970@users.noreply.github.com>
2023-11-02 19:01:00 +0530

    script.js

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 function setup() {
-  createCanvas(400, 400);
-  background(200);
-  drawTulip(width / 2, height / 2);
-}
-
-function drawTulip(x, y) {
-  // Stem
-  fill(34, 139, 34);
-  rect(x - 5, y, 10, 100);
-
-  // Leaves
-  fill(0, 128, 0);
-  ellipse(x - 10, y + 20, 30, 20);
-  ellipse(x + 10, y + 20, 30, 20);
-
-  // Flower
-  fill(255, 0, 0);
-  ellipse(x, y, 40, 40);
-  fill(255, 192, 203);
-  ellipse(x - 15, y, 30, 30);
-  ellipse(x + 15, y, 30, 30);
-}
+    createCanvas(400, 400);
+    background(200);
+    drawBouquet(500); // Draw a total of 25 flowers
+  }
+  
+  function drawBouquet(numFlowers) {
+    for (let i = 0; i < numFlowers; i++) {
+      let x = random(50, width - 50);
+      let y = random(100, height - 50);
+      drawFlower(x, y);
+    }
+  }
+  
+  function drawFlower(x, y) {
+    // Stem
+    fill(34, 139, 34);
+    rect(x - 5, y, 10, 100);
+  
+    // Flower
+    fill(255, 0, 0);
+    ellipse(x, y - 20, 40, 40);
+    ellipse(x - 20, y, 40, 40);
+    ellipse(x + 20, y, 40, 40);
+  
+    // Center
+    fill(255, 255, 0);
+    ellipse(x, y, 20, 20);
+  }
+  
